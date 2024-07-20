34a61d210320a351de0efd979d9c54aeb9e6511a
Pat Luo <patrick.luong615@gmail.com>
2024-07-20 02:54:34 -0400

    Update dark mode styles for leaflet controls

--- components/ui/map.tsx
+++ components/ui/map.tsx
@@ -12,12 +12,16 @@ const { BaseLayer } = LayersControl;
 import "leaflet/dist/leaflet.css";
 import { FullActivity } from "@/lib/types";
 import polyline from "@mapbox/polyline";
+import { useTheme } from "next-themes";
 
 interface MapProps {
 	activities: FullActivity[];
 }
 
 export default function Map({ activities }: MapProps) {
+	const { resolvedTheme } = useTheme();
+	const isDarkMode = resolvedTheme === "dark";
+
 	const polylines: [number, number][][] = activities.map((activity) => {
 		return polyline.decode(activity.map.summary_polyline);
 	});
@@ -34,28 +38,22 @@ export default function Map({ activities }: MapProps) {
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
 			<LayersControl position="bottomleft">
-				<BaseLayer name="Light">
+				<BaseLayer checked={!isDarkMode} name="Light">
 					<TileLayer
 						attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 						url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
 						maxZoom={20}
 					/>
 				</BaseLayer>
-				<BaseLayer checked name="Dark">
+				<BaseLayer checked={isDarkMode} name="Dark">
 					<TileLayer
 						attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 						url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
 						maxZoom={20}
 					/>
 				</BaseLayer>
-				<BaseLayer name="Colorful">
-					<TileLayer
-						attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-						url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
-						maxZoom={20}
-					/>
-				</BaseLayer>
-				<BaseLayer name="Satellite">
+
+				<BaseLayer checked={false} name="Satellite">
 					<TileLayer
 						attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
 						url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
@@ -64,7 +62,7 @@ export default function Map({ activities }: MapProps) {
 			</LayersControl>
 			{polylines.map((polyline, i) => (
 				<Polyline
-					className="opacity-50"
+					className="opacity-30"
 					key={i}
 					positions={polyline}
 					pathOptions={{ color: "hsl(24.6, 95%, 53.1%)" }}
