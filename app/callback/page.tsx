3f8de4d7b3fa99dcc2c8461e9ce0da4d6677d34f
Pat Luo <patrick.luong615@gmail.com>
2024-07-25 17:27:37 -0400

    style redirect page

--- app/callback/page.tsx
+++ app/callback/page.tsx
@@ -1,37 +1,43 @@
 "use client";
 import { useEffect } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
+import { MoonLoader } from "react-spinners";
 
 export default function Callback() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const authCode = searchParams.get("code");
+	const router = useRouter();
+	const searchParams = useSearchParams();
+	const authCode = searchParams.get("code");
 
-  useEffect(() => {
-    const fetchToken = async () => {
-      if (authCode) {
-        try {
-          const response = await fetch(
-            "http://localhost:3000/api/exchange-auth-code",
-            {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({ authCode }),
-            }
-          );
-          if (response.ok) {
-            router.push("/");
-          }
-        } catch (error) {
-          console.error("Error exchanging token:", error);
-        }
-      }
-    };
+	useEffect(() => {
+		const fetchToken = async () => {
+			if (authCode) {
+				try {
+					const response = await fetch(
+						"http://localhost:3000/api/exchange-auth-code",
+						{
+							method: "POST",
+							headers: {
+								"Content-Type": "application/json",
+							},
+							body: JSON.stringify({ authCode }),
+						}
+					);
+					if (response.ok) {
+						router.push("/");
+					}
+				} catch (error) {
+					console.error("Error exchanging token:", error);
+				}
+			}
+		};
 
-    fetchToken();
-  }, []);
+		fetchToken();
+	}, []);
 
-  return <div>Redirecting...</div>;
+	return (
+		<div className="h-screen flex items-center justify-center">
+			<p className="text-2xl">Redirecting...</p>
+			<MoonLoader color="#f8701b" loading={true} size={50} />
+		</div>
+	);
 }
